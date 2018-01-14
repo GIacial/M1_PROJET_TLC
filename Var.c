c93f634a73c6429926ec2d0bd6044fcbd99b87fb
morganethi <morgane.thielemann@etu.univ-poitiers.fr>
2018-01-14 16:07:17 +0100

    var.c

--- Var.c
+++ Var.c
@@ -1,1 +1,85 @@
 #include "Var.h"
+
+//------------------------------------------------------------------------
+struct Var {
+	Text nom;
+	void* val; 		// Pour les types primitifs
+	File variables; // Pour les objets complexes
+	Type type;
+};
+//------------------------------------------------------------------------
+
+Var createVar(Type t , Text nom , File variables){
+	Var res = (Var)malloc(sizeof(struct Var));
+	res->nom = nom;
+	res->type = t;
+	res->val = NULL;
+	res->variables = variables;
+	return res;
+}
+
+//------------------------------------------------------------------------
+void freeVar(Var* pSurVar){
+	freeText(&((*pSurVar)->nom));
+	if((*pSurVar)->val !=NULL){
+		free((*pSurVar)->val);
+	}
+	if(!emptyFile((*pSurVar)->variables)){
+		Iterator i = getIteratorFile((*pSurVar)->variables);
+		while(hasNextIterator(i)){
+			Var v = (Var)nextDataIterator(i);
+			freeVar(&v);
+		}
+		freeIterator(&i);
+		freeFile(&((*pSurVar)->variables));
+	}
+	freeType(&((*pSurVar)->type));
+	free(*pSurVar);
+	(*pSurVar) = NULL;
+}
+
+//------------------------------------------------------------------------
+
+Type getTypeVar(Var v) {
+	return v->type;
+}
+
+//------------------------------------------------------------------------
+
+void* 	getValueVar(Var v){
+	return v->val;
+}
+
+//------------------------------------------------------------------------
+
+Var  getVarInVar(Var v , Text nomVar){
+	if(!emptyFile((*pSurVar)->variables)){
+		Iterator i = getIteratorFile(v->variables);
+		while(hasNextIterator(i)){
+			Var v = (Var)nextDataIterator(i);
+			if(isEgalText(nomVar,v->nom)){
+				freeIterator(&i);
+				return v;
+			}
+		}
+		freeIterator(&i);
+		return NULL;
+	}
+}
+
+//------------------------------------------------------------------------
+
+bool  isMyNameVar(Var v,Text nom){
+	return isEgalText(nom,v->nom);
+}
+
+//------------------------------------------------------------------------
+
+Text getNameVar(Var v){
+	return v->nom;
+}
+
+//------------------------------------------------------------------------
+bool isSameVar(Var v,Var g){
+	return isEgalText(v->nom,g->nom);
+}
